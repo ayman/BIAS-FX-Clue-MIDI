e1aecd9c2af5e08ea437a72a5fb8e844acac4396
David A. Shamma <1846+ayman@users.noreply.github.com>
2021-12-24 22:09:09 -0800

    Adding a better debouncer for the buttons that is non-blocking.

--- code.py
+++ code.py
@@ -41,13 +41,25 @@ clue.display.brightness = 1.0
 clue.pixel.brightness = 0.2
 
 
+BUTTONS = {
+    "MODE": "MODE",
+    "PROXY": "PROXY",
+    "CC_0": "CC_0",
+    "CC_1": "CC_1",
+    "CC_2": "CC_2",
+    "CC_MINUS": "CC_MINUS",
+    "CC_PLUS": "CC_PLUS",
+    "CALIBRATE": "CALIBRATE"
+}
+
+
 class Debouncer:
-    def __init__(self, n=6, t=0.75):
+    def __init__(self, items, t=0.75):
         "A debouncer that has a timeout for buttons."
         stamp = time.monotonic()
         self.buttons = {}
         self.timeout = t
-        for i in range(n):
+        for i in items:
             self.buttons[i] = stamp
 
     def hot(self, button, press=True):
@@ -60,7 +72,7 @@ class Debouncer:
         return True
 
 
-class BiasClueDisplay:
+class DisplayManager:
     """ Control the display.
     """
 
@@ -77,7 +89,7 @@ class BiasClueDisplay:
             0, 210, 240, 4, fill=COLORS["GREEN"], outline=None)
         self.screen.append(bottom_trim_box)
         footer_box = Rect(
-            0, 215, 240, 50, fill=COLORS["GREEN_DARK"], outline=None)
+            0, 214, 240, 50, fill=COLORS["GREEN_DARK"], outline=None)
         self.screen.append(footer_box)
 
         self.top_label = label.Label(terminalio.FONT,
@@ -116,7 +128,7 @@ class BiasClueDisplay:
             line_spacing=0.8,
         )
         self.b_label.anchor_point = (0.5, 0.5)
-        self.b_label.x = 220
+        self.b_label.x = 222
         self.b_label.y = 170
         self.b_label.label_direction = "UPR"
 
@@ -127,7 +139,7 @@ class BiasClueDisplay:
             color=COLORS["SILVER"],
         )
         self.patch_label.x = 4
-        self.patch_label.y = 225
+        self.patch_label.y = 224
 
         self.footer_label = label.Label(
             terminalio.FONT,
@@ -136,7 +148,7 @@ class BiasClueDisplay:
             color=COLORS["WHITE"],
         )
         self.footer_label.x = 240 - 60
-        self.footer_label.y = 230
+        self.footer_label.y = 228
 
         self.init_screen()
         self.screen.append(self.title_label)
@@ -196,33 +208,15 @@ class BiasClueDisplay:
             self.init_screen()
 
 
-def remap(x, o_min, o_max, n_min, n_max):
-    # range check
-    if o_min == o_max:
-        return int(x)
-    if n_min == n_max:
-        return int(x)
-    # check reversed input range
-    reverse_input = False
-    old_min = min(o_min, o_max)
-    old_max = max(o_min, o_max)
-    if not old_min == o_min:
-        reverse_input = True
-    # check reversed output range
-    reverse_output = False
-    new_min = min(n_min, n_max)
-    new_max = max(n_min, n_max)
-    if not new_min == n_min:
-        reverse_output = True
-    portion = (x-old_min)*(new_max-new_min)/(old_max-old_min)
-    if reverse_input:
-        portion = (old_max-x)*(new_max-new_min)/(old_max-old_min)
-    result = portion + new_min
-    if reverse_output:
-        result = new_max - portion
-    if result < 0:
-        result = 0
-    return int(result)
+def remap(x, in_min, in_max, out_min, out_max, no_float=True):
+    res = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    if res < out_min:
+        res = out_min
+    if res > out_max:
+        res = out_max
+    if no_float:
+        res = round(res)
+    return res
 
 
 MIDI_CHANNEL = 1
@@ -241,82 +235,89 @@ BLE.name = "BIAS FX CLUE BLE MIDI"
 print("Advertising BLE")
 BLE.start_advertising(BLE_AD)
 
-bias_clue = BiasClueDisplay(clue)
-debouncer = Debouncer()
-start_range = 0
+clue_display = DisplayManager(clue)
+debouncer = Debouncer(BUTTONS)
+START_RANGE = 0
 
 while True:
     print("Waiting for connection")
     while not BLE.connected:
         pass
     print("Connected")
-    bias_clue.patch_label.text = "Connecting"
+    clue_display.patch_label.text = "Connecting"
     clue.white_leds = True
     time.sleep(0.5)
     clue.white_leds = False
-    bias_clue.patch_label.text = "Connected!"
+    clue_display.patch_label.text = "Connected!"
     time.sleep(1.0)
-    bias_clue.show_screen(0)
+    clue_display.show_screen(0)
     while BLE.connected:
         if clue.button_a:
-            if not debouncer.hot(4):
-                bias_clue.next_screen()
+            if not debouncer.hot(BUTTONS["MODE"]):
+                clue_display.next_screen()
 
-        if bias_clue.current_screen == 0:
+        ACCEL_DATA = clue.acceleration
+        PROX_DATA = clue.proximity
+        ACCEL_X = ACCEL_DATA[0]
+
+        if clue_display.current_screen == 0:
             midi_data = []
-            ACCEL_DATA = clue.acceleration
-            PROX_DATA = clue.proximity
-            ACCEL_X = ACCEL_DATA[0]
             # Remap analog readings to cc range
             cc_x = int(simpleio.map_range(ACCEL_X, -9, 9, 0, 127))
-            cc_x = remap(cc_x, start_range, 127, 0, 127)
-            midi_data.append(ControlChange(bias_clue.starting_patch + 4, cc_x))
+            cc_x = remap(cc_x, START_RANGE, 127, 0, 127)
+            midi_data.append(ControlChange(
+                clue_display.starting_patch + 4, cc_x))
             # It's easier to map it inverted for a shoe mount...
             # CC_Y = int(simpleio.map_range(ACCEL_Y, -9, 9, 0, 127))
             if clue.white_leds:
                 clue.white_leds = False
             CC_PROX = int(simpleio.map_range(clue.proximity, 0, 255, 0, 127))
             CC_PROX_SWITCH = 127 if CC_PROX > 4 else 0
-            if CC_PROX_SWITCH != 0 and not debouncer.hot(3):
+            if CC_PROX_SWITCH != 0 and not debouncer.hot(BUTTONS["PROXY"]):
                 clue.white_leds = True
-                midi_data.append(ControlChange(bias_clue.starting_patch + 3,
+                midi_data.append(ControlChange(clue_display.starting_patch + 3,
                                                CC_PROX_SWITCH))
             if clue.touch_0:
-                if not debouncer.hot(0):
+                if not debouncer.hot(BUTTONS["CC_0"]):
                     print("Touch 0")
-                    midi_cc = ControlChange(bias_clue.starting_patch, 0)
+                    midi_cc = ControlChange(clue_display.starting_patch, 0)
                     midi_data.append(midi_cc)
             if clue.touch_1:
-                if not debouncer.hot(1):
+                if not debouncer.hot(BUTTONS["CC_1"]):
                     print("Touch 1")
-                    midi_cc = ControlChange(bias_clue.starting_patch + 1, 0)
+                    midi_cc = ControlChange(clue_display.starting_patch + 1, 0)
                     midi_data.append(midi_cc)
             if clue.touch_2:
-                if not debouncer.hot(2):
+                if not debouncer.hot(BUTTONS["CC_2"]):
                     print("Touch: 2")
-                    midi_cc = ControlChange(bias_clue.starting_patch + 2, 0)
+                    midi_cc = ControlChange(clue_display.starting_patch + 2, 0)
                     midi_data.append(midi_cc)
             if len(midi_data) > 0:
                 MIDI.send(midi_data)
-            time.sleep(0.1)
+            time.sleep(0.01)
 
-        if bias_clue.current_screen == 1:
+        if clue_display.current_screen == 1:
             if CC_PROX_SWITCH != 0:
-                clue.white_leds = True
-                time.sleep(DEBOUNCE_DELAY)
-                clue.white_leds = False
+                if not debouncer.hot(BUTTONS["PROXY"]):
+                    clue.white_leds = True
+                    time.sleep(DEBOUNCE_DELAY)
+                    clue.white_leds = False
             if clue.touch_0:
-                print("Touch: -")
-                bias_clue.cc_dec()
-                time.sleep(DEBOUNCE_DELAY)
+                if not debouncer.hot(BUTTONS["CC_MINUS"]):
+                    print("Touch: -")
+                    clue_display.cc_dec()
+                    time.sleep(DEBOUNCE_DELAY)
             if clue.touch_1:
-                print("Touch: +")
-                bias_clue.cc_inc()
-                time.sleep(DEBOUNCE_DELAY)
+                if not debouncer.hot(BUTTONS["CC_PLUS"]):
+                    print("Touch: +")
+                    clue_display.cc_inc()
+                    time.sleep(DEBOUNCE_DELAY)
             if clue.touch_2:
-                print("Touch: *")
-                start_range = int(simpleio.map_range(ACCEL_X, -9, 9, 0, 127))
-                time.sleep(DEBOUNCE_DELAY)
+                if not debouncer.hot(BUTTONS["CALIBRATE"]):
+                    print("Touch: *")
+                    START_RANGE = int(
+                        simpleio.map_range(ACCEL_X, -9, 9, 0, 127))
+                    time.sleep(DEBOUNCE_DELAY)
     print("Disconnected")
-    bias_clue.init_screen()
+    clue_display.init_screen()
     BLE.start_advertising(BLE_AD)
